a1566ee927d9ca82cd341427e250a2cd51e58f13
Erin Bell <ebellempire@gmail.com>
2020-06-16 10:28:26 -0400

    formatting

--- TimelineShortcodePlugin.php
+++ TimelineShortcodePlugin.php
@@ -17,8 +17,8 @@ class TimelineShortcodePlugin extends Omeka_Plugin_AbstractPlugin
 
 
 	/*
-    ** Plugin options
-    */
+	** Plugin options
+	*/
 
 	public function hookConfigForm()
 	{
@@ -53,57 +53,57 @@ class TimelineShortcodePlugin extends Omeka_Plugin_AbstractPlugin
 	 * Add Shortcode.
 	 */
 
-    public function hookInitialize()
-    {
-        add_shortcode('timeline_list', array($this, 'ts_timelineList'));
-    }
-    
-    public function ts_timelineList($args, $view)
-    {
-        // converts shortcode for display
-        return '<div id="ts-widget-container">'.ts_display_timeline_list($args).'</div>';
-    }	
-    
+	public function hookInitialize()
+	{
+		add_shortcode('timeline_list', array($this, 'ts_timelineList'));
+	}
+
+	public function ts_timelineList($args, $view)
+	{
+		// converts shortcode for display
+		return '<div id="ts-widget-container">'.ts_display_timeline_list($args).'</div>';
+	}
+
 	public static function getElementFields(){
 		$elements=array();
 		foreach (get_record('ElementSet',array('name'=>"Dublin Core"))->getElements() as $element){
 			$elements['Dublin Core,'.$element->name]='Dublin Core: '.$element->name;
-		}	
+		}
 		foreach (get_record('ElementSet',array('name'=>"Item Type Metadata"))->getElements() as $element){
 			$elements['Item Type Metadata,'.$element->name]='Item Type Metadata: '.$element->name;
-		}			
-		return $elements;	
-	}	    
-	
-}	
+		}
+		return $elements;
+	}
+
+}
 
 
 
 function ts_display_timeline_list($args){
-	
+
 	// plugin settings
 	$pluginSettings = explode(',',get_option('ts_date_field'));
 	$dateSet= $pluginSettings[0];
-	$dateElement=$pluginSettings[1];	
-	
+	$dateElement=$pluginSettings[1];
+
 	// shortcode arguments
 	$element = isset($args['element']) ? $args['element'] : false;
 	$value = isset($args['value']) ? $args['value'] : false;
 	$featured = isset($args['featured']) ? $args['featured'] : null; // value doesn't matter
 
-	if($element && $value){		
+	if($element && $value){
 		// Do something...
-		$query=array('featured'=>$featured,'advanced' => array ( 
+		$query=array('featured'=>$featured,'advanced' => array (
 				array(
-			       'element_id' => ts_get_element_id($element), 
-			       'type' => 'is exactly', 
-			       'terms' => $value,
-			       )
-			    ));
+					  'element_id' => ts_get_element_id($element),
+					  'type' => 'is exactly',
+					  'terms' => $value,
+					  )
+				   ));
 
 		$items=get_records('Item',$query,100);
 		if(count($items)>0){
-			
+
 			$html='<style type="text/css">
 				.timeline-year{
 					font-size: 200%;
@@ -133,13 +133,13 @@ function ts_display_timeline_list($args){
 					margin: 0 1em 0 0;
 					vertical-align: bottom;
 				}
-			</style>';			
-			
+			</style>';
+
 			$itemsByDate=array();
 			foreach($items as $item){
-				
+
 				$date=metadata($item,array($dateSet, $dateElement));
-				
+
 				if( isset($date) ){
 					$itemData=array();
 					$itemData['id']=$item->id;
@@ -148,25 +148,25 @@ function ts_display_timeline_list($args){
 					$itemData['date_text']=$date;
 					$itemData['credit']= metadata($item,array('Dublin Core','Creator')) ? metadata($item,array('Dublin Core','Creator')) : (metadata($item,array('Dublin Core','Contributor')) ? metadata($item,array('Dublin Core','Contributor')) : 'Unknown');
 					$itemData['thumbnail'] = metadata($item,'has_files') ? record_image($item,'square_thumbnail') : null;
-					
+
 					// push content to array with sortable date keys
-					$itemsByDate[ts_date_format('Ymd',$date)][] = $itemData;					
+					$itemsByDate[ts_date_format('Ymd',$date)][] = $itemData;
 				}
-				
+
 			}
 			// sort items array by date key
-			ksort($itemsByDate); 
-			
-			
+			ksort($itemsByDate);
+
+
 			$yearsAlreadyDisplayed=array();
 			foreach($itemsByDate as $dateKey=>$items){
 				if(!in_array(ts_date_format('Y',$dateKey), $yearsAlreadyDisplayed)){
 					$html.= '<div class="timeline-year year-id-'.ts_date_format('Y',$dateKey).'">'.ts_date_format('Y',$dateKey).'</div>';
-					    if(!in_array(ts_date_format('Y',$dateKey), $yearsAlreadyDisplayed, true)){
-					        array_push($yearsAlreadyDisplayed, ts_date_format('Y',$dateKey));
-					    }
+						 if(!in_array(ts_date_format('Y',$dateKey), $yearsAlreadyDisplayed, true)){
+							 array_push($yearsAlreadyDisplayed, ts_date_format('Y',$dateKey));
+						 }
 				}
-				
+
 				$html.= '<div id="date-'.$dateKey.'" class="timeline-section">';
 				$i=1;
 				foreach($items as $item){
@@ -180,12 +180,12 @@ function ts_display_timeline_list($args){
 			}
 			return $html;
 
-			
+
 		}else{
 			echo '...';
 		};
-		
-		
+
+
 	}else{
 		echo '<strong>Timeline Shortcode Error:</strong> Missing required arguments. Required arguments are element and value.<br>Example: [timeline_list element=\'Subject\' value=\'the subject to match\']<br>Debug: '.json_encode($args);
 	}
@@ -213,30 +213,30 @@ Supported date formats:
 function ts_date_format($format,$date){
 	// ABT: always be trimming
 	$date=trim($date);
-	
+
 	// First check to see if we're working with one date or a date span
 	// ...for spans, we'll only use the first date
 	$checkDate=explode('–',$date); // endash
 	if(count($checkDate)==2){
-		$date=$checkDate[0]; 
+		$date=$checkDate[0];
 		$date=trim($date);
 	}
 	$checkDate2=explode('-',$date); // hyphen
 	if(count($checkDate2)==2){
-		$date=$checkDate2[0]; 
+		$date=$checkDate2[0];
 		$date=trim($date);
-	}	
+	}
 	// Remove circa
-	$date = str_replace('circa','',$date); 
+	$date = str_replace('circa','',$date);
 	$date=trim($date);
-	
+
 	// Add january 1st to standalone 4-digit years (otherwise PHP will automatically add today's month and day)
 	if( is_numeric($date) && strlen($date) == 4 ){
-		$date = '01/01/'.$date; 
+		$date = '01/01/'.$date;
 	}
-	
+
 	// Let PHP do the rest of the work...
 	$date = date($format,strtotime($date));
-	
+
 	return $date;
 }
